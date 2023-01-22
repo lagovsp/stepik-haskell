a5f393e2eed528d4299b75e7abbc5e59e5efb12a
lagovsp <lagovsp@gmail.com>
2023-01-22 08:54:53 +0300

    qsort fixed ++ -> :

--- 3-lists/qsort.hs
+++ 3-lists/qsort.hs
@@ -1,7 +1,7 @@
 qsort :: Ord a => [a] -> [a]
 qsort []    = []
 qsort [x,y] = if x < y then [x,y] else [y,x]
-qsort xs    = left ++ [head xs] ++ right where
+qsort xs    = left ++ head xs : right where
   isLowerThanFst = (< head xs)
   left           = qsort $ filter isLowerThanFst xs
   right          = qsort $ filter (not . isLowerThanFst) (tail xs)
